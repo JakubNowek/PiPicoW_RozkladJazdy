c514a4fac255e784c16697d94e677bc5b9a96ac2
JakubNowek <62256934+JakubNowek@users.noreply.github.com>
2022-10-19 18:21:46 +0200

    added new ili9341 library
    this library provides wide range of functions

--- PiPicoW/ili9341.py
+++
@@ -1,9 +0,0 @@
-import ili934x as ili9341
-from machine import Pin, SPI
-
-led = Pin(21, Pin.OUT)
-led.high()
-spi = SPI(id=0, miso=Pin(16), mosi=Pin(19, Pin.OUT), sck=Pin(6, Pin.OUT))
-display = ili9341.ILI9341(spi, cs=Pin(0), dc=Pin(5), rst=Pin(4))
-display.fill(ili9341.color565(0xff, 0x11, 0x22))
-display.print('text\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\ntext\n')
